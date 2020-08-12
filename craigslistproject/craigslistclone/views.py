28eabed9cbb3178dcea7cb4e2f3773a5aa57f26d
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-08-13 00:28:50 +0100

    Search functionality
    This is for accurately bringing the result of the search

--- craigslistproject/craigslistclone/views.py
+++ craigslistproject/craigslistclone/views.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from . import models
 
 BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/bbb?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
 def home(request):
@@ -21,14 +22,32 @@ def new_search(request):
     soup = BeautifulSoup(data,features='html.parser')
 
     post_listings = soup.find_all('li', {'class': 'result-row'})
-    post_title = post_listings[0].find(class_='result-title').text
-    post_url = post_listings.find('a').get('href')
-    post_price = post_listings[0].find(class='result-price').text
+    
+    final_postings = []
+    
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        post_price = post.find(class_='result-price')
+
+        
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_image_url))
+   
 
     #print(data)
 
     print(search)
+
     stuff_for_frontend = {
-        'search':search,
+        'search': search,
+        'final_postings': final_postings,
     }
-    return render(request,'craigslistclone/new_search.html',stuff_for_frontend)    
+    return render(request,'craigslistclone/new_search.html', stuff_for_frontend)    
