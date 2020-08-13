12ac692bcfb6c2b84221a81aaa05ff5126b93852
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-08-13 14:02:19 +0100

    search functionality

--- craigslistproject/craigslistproject/settings.py
+++ craigslistproject/craigslistproject/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-#django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = (os.path.join(BASE_DIR,'static'))
+
+#django_heroku.settings(locals())
